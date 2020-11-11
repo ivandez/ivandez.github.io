5915c6a3754faf559b5fb2547c7b77041adb8f45
Ivan Mendez <ivanmr932@gmail.com>
2020-11-11 15:57:37 -0400

    create work experiencie

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/navbar/NavBar";
 import Hero from "./components/hero/Hero";
 import Container from "./components/container/Container";
 import AboutMe from "./components/about/AboutMe";
+import WorkExperience from "./components/work/WorkExperience";
 
 function App() {
   return (
@@ -11,6 +12,14 @@ function App() {
       <Hero></Hero>
       <Container title="Sobre mí">
         <AboutMe></AboutMe>
+      </Container>
+      <Container title="Experiencia laboral">
+        <WorkExperience
+          date="Oct 2019 – Mar 2020"
+          company="Founduss"
+          job="desarrollador backend con laravel"
+          jobDescription="Cree un sistema automatizado de reserva de servicios empresariales, cree una BBDD SQL, cree una API rest para una app movil."
+        ></WorkExperience>
       </Container>
     </div>
   );
