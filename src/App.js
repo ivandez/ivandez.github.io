6f0c876172b1b93f22b97aa2bc171f428c52e02d
Ivan Mendez <ivanmr932@gmail.com>
2020-11-11 16:18:35 -0400

    created abilities

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from "./components/container/Container";
 import AboutMe from "./components/about/AboutMe";
 import WorkExperience from "./components/work/WorkExperience";
 import Education from "./components/education/Education";
+import Abilities from "./components/abilities/Abilities";
 
 function App() {
   return (
@@ -33,6 +34,27 @@ function App() {
           date="2018 - presente"
           degree="Técnico Superior en Informática"
         />
+      </Container>
+      <Container title="Habilidades">
+        <Abilities
+          abilities={[
+            "HTML5",
+            "CSS3",
+            "JavaScript",
+            "Boostrap 4",
+            "React.js",
+            "Angular.js",
+            "PHP",
+            "Laravel",
+            "SQL/MySQL Workbench",
+            "Git/Github",
+            "Inglés",
+            "Ubuntu",
+            "Vagrant/VirtualBox",
+            "MVC",
+            "Programación Orientada a Objectos",
+          ]}
+        ></Abilities>
       </Container>
     </div>
   );
---
+++ src/components/abilities/Abilities.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./abilities.css";
+
+const Abilities = (props) => {
+  const listAbilities = props.abilities.map((element) => (
+    <li className="abilities__item">{element}</li>
+  ));
+
+  return (
+    <div className="abilities">
+      <ul className="abilities__list">{listAbilities}</ul>
+    </div>
+  );
+};
+
+export default Abilities;
