d7dec5c189a517a3bafc5923367efb83b5deb1c5
Ivan Mendez <ivanmr932@gmail.com>
2020-11-15 21:06:45 -0400

    add new feature to container.js
    If container containts more than 5 childs, will disappear them and add a show more button

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./container.css";
 
 const Container = (props) => {
-  return (
-    <div className="container">
-      <h1 className="container__title">{props.title}</h1>
-      {props.children}
-    </div>
-  );
+  const [open, setIsOpen] = useState(false);
+  let className = "hide-children";
+
+  if (open === true) {
+    className = "show-children ";
+  }
+
+  let childrenCount = React.Children.count(props.children);
+  let children = [];
+  let childrenToHide = [];
+  let childrenArray = [];
+
+  if (childrenCount > 5) {
+    childrenArray.push(props.children);
+    //can refactor with splice method
+    for (let i = 0; i < childrenCount; i++) {
+      if (i < 5) {
+        children.push(childrenArray[0][i]);
+      } else {
+        childrenToHide.push(childrenArray[0][i]);
+      }
+    }
+
+    return (
+      <div className="container">
+        <h1 className="container__title">{props.title}</h1>
+        {children}
+        <div className={className}>{childrenToHide}</div>
+        <button onClick={() => setIsOpen(!open)}>ver mas</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <h1 className="container__title">{props.title}</h1>
+        {props.children}
+      </div>
+    );
+  }
 };
 
 export default Container;
