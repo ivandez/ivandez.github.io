50858c601406665b57303330acf9786825de5ba0
Ivan Mendez <ivanmr932@gmail.com>
2020-11-21 14:25:22 -0400

    fixed show more in container

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -4,9 +4,10 @@ import "./container.css";
 const Container = (props) => {
   const [open, setIsOpen] = useState(false);
   let className = "hide-children";
-
+  let texto = "Mostrar más";
   if (open === true) {
     className = "show-children ";
+    texto = "Mostrar menos";
   }
 
   let childrenCount = React.Children.count(props.children);
@@ -30,6 +31,14 @@ const Container = (props) => {
         <h1 className="container__title">{props.title}</h1>
         {children}
         <div className={className}>{childrenToHide}</div>
+        <div className="container__show-more">
+          <button
+            className="container__button"
+            onClick={() => setIsOpen(!open)}
+          >
+            {texto + ` (${childrenCount - 5})`}
+          </button>
+        </div>
       </div>
     );
   } else {
