d6c593e663dcac9064eaef0db6d76241d498a5b1
Ivan Mendez <ivanmr932@gmail.com>
2020-11-23 17:47:06 -0400

    end responsive styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,6 +6,7 @@ import {
   faLinkedin,
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
+import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   const year = new Date().getFullYear();
@@ -14,8 +15,14 @@ function Footer() {
       <div className="footer__owner">
         <p>Iván Méndez {year}</p>
       </div>
-      <div>
-        <ul className="footer__list">
+      <div className="footer__up-arrow">
+        <FontAwesomeIcon
+          icon={faArrowCircleUp}
+          className="footer__social"
+        ></FontAwesomeIcon>
+      </div>
+      <div className="footer__socials">
+        {/* <ul className="footer__list">
           <li className="footer__item">
             <a
               className="footer__link"
@@ -55,7 +62,40 @@ function Footer() {
               ></FontAwesomeIcon>
             </a>
           </li>
-        </ul>
+        </ul> */}
+        <a
+          className="footer__link"
+          href="https://www.linkedin.com/in/iv%C3%A1n-m%C3%A9ndez-rodr%C3%ADguez-43b6b2175/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className="footer__social"
+          ></FontAwesomeIcon>
+        </a>
+        <a
+          className="footer__link"
+          href="https://www.instagram.com/ivan_mendez_dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="footer__social"
+          ></FontAwesomeIcon>
+        </a>
+        <a
+          className="footer__link"
+          href="https://github.com/ivandez"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="footer__social"
+          ></FontAwesomeIcon>
+        </a>
       </div>
     </div>
   );
