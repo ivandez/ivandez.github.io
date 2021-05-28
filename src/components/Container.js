675f62cff085740a142c899ccde4dda78b55cd08
Ivan Mendez <ivanmr932@gmail.com>
2021-05-27 22:59:08 -0400

    Working in progress in Container.js
    I have to create a function to handle when Container.js has more than 5 children

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,37 +2,43 @@ import React, { useState } from "react";
 
 const Container = (props) => {
   const [open, setIsOpen] = useState(false);
-  let className = "hide-children";
+  // let className = "hide-children";
   let texto = "Mostrar más";
-  if (open === true) {
-    className = "show-children ";
-    texto = "Mostrar menos";
-  }
+  // if (open === true) {
+  //   className = "show-children ";
+  //   texto = "Mostrar menos";
+  // }
 
   let childrenCount = React.Children.count(props.children);
-  let children = [];
-  let childrenToHide = [];
-  let childrenArray = [];
+  // let children = [];
+  // let childrenToHide = [];
+  let childrenToHide = props.children.slice(5);
+  let childrenToShow = props.children.slice(0, 5);
+
+  // let childrenArray = [];
 
   if (childrenCount > 5) {
-    childrenArray.push(props.children);
-    //can refactor with splice method
-    for (let i = 0; i < childrenCount; i++) {
-      if (i < 5) {
-        children.push(childrenArray[0][i]);
-      } else {
-        childrenToHide.push(childrenArray[0][i]);
-      }
-    }
+    // childrenArray.push(props.children);
+    // //can refactor with splice method
+    // for (let i = 0; i < childrenCount; i++) {
+    //   if (i < 5) {
+    //     children.push(childrenArray[0][i]);
+    //   } else {
+    //     childrenToHide.push(childrenArray[0][i]);
+    //   }
+    // }
 
     return (
-      <div className="container" id={props.id}>
-        <h1 className="container__title">{props.title}</h1>
-        {children}
-        <div className={className}>{childrenToHide}</div>
-        <div className="container__show-more">
+      <div className="container centerContainer" id={props.id}>
+        <h1 className="container__title text-white text-3xl font-bold">{props.title}</h1>
+        {/* {children} */}
+        {childrenToShow}
+
+        {/* <div className="hidden">{childrenToHide}</div> */}
+        <div className={open ? "block" : "hidden"}>{childrenToHide}</div>
+        <div className="container__show-more text-center">
           <button
-            className="container__button"
+            className="container__button text-white"
             onClick={() => setIsOpen(!open)}
           >
             {texto + ` (${childrenCount - 5})`}
@@ -41,9 +47,23 @@ const Container = (props) => {
       </div>
     );
   } else {
+    // return (
+    //   <div className="container centerContainer" id={props.id}>
+    //     <h1 className="container__title text-white text-3xl font-bold">
+    //       {props.title}
+    //     </h1>
+    //     {props.children}
+    //   </div>
+    // );
+    return HasLessThatFiveChildren(props)
+  }
+
+  function HasLessThatFiveChildren(props) {
     return (
       <div className="container centerContainer" id={props.id}>
-        <h1 className="container__title text-white text-3xl font-bold">{props.title}</h1>
+        <h1 className="text-white text-3xl font-bold">
+          {props.title}
+        </h1>
         {props.children}
       </div>
     );
@@ -51,3 +71,7 @@ const Container = (props) => {
 };
 
 export default Container;
+
+// TODO
+//   1 refactorizar con el hook como se imprimi los elementos LISTO
+//   2 refactorizar con splice como se dividen los elementos LISTO
