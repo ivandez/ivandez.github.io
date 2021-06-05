11f2d905afde9754f8f72f27b6554f10ac70f379
Ivan Mendez <ivanmr932@gmail.com>
2021-06-04 21:49:32 -0400

    End footer refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,48 +12,50 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <div className="bg-white text-center py-2">
+        <div className="lg:grid lg:grid-cols-3 lg:py-5">
       <div>
         <p>Iván Méndez {year}</p>
       </div>
-        <div className="my-2">
-          <FontAwesomeIcon
-            onClick={() => scroll.scrollToTop(1000)}
-            icon={faArrowCircleUp}
-            className="text-xl cursor-pointer hover:text-myColor transition duration-300"
-          ></FontAwesomeIcon>
-        </div>
-        <div className="flex justify-evenly space-b">
-          <a
-            href="https://www.linkedin.com/in/iv%C3%A1n-m%C3%A9ndez-rodr%C3%ADguez-43b6b2175/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <div className="my-2 text-center">
             <FontAwesomeIcon
-              icon={faLinkedin}
-              className="text-2xl"
+              onClick={() => scroll.scrollToTop(1000)}
+              icon={faArrowCircleUp}
+              className="text-xl cursor-pointer hover:text-myColor transition duration-300 lg:text-3xl"
             ></FontAwesomeIcon>
-          </a>
-          <a
-            href="https://www.instagram.com/ivan_mendez_dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              className="text-2xl"
-            ></FontAwesomeIcon>
-          </a>
-          <a
-            href="https://github.com/ivandez"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faGithub}
-              className="text-2xl"
-            ></FontAwesomeIcon>
-          </a>
-        </div>
+          </div>
+          <div className="flex justify-evenly space-b">
+            <a
+              href="https://www.linkedin.com/in/iv%C3%A1n-m%C3%A9ndez-rodr%C3%ADguez-43b6b2175/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="text-2xl lg:text-3xl"
+              ></FontAwesomeIcon>
+            </a>
+            <a
+              href="https://www.instagram.com/ivandez_dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="text-2xl lg:text-3xl"
+              ></FontAwesomeIcon>
+            </a>
+            <a
+              href="https://github.com/ivandez"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="text-2xl lg:text-3xl"
+              ></FontAwesomeIcon>
+            </a>
+          </div>
+          </div>
     </div>
   );
 }
